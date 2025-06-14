1f6ca8445e48810e7ca9eae9bd81cdc27482f836
hayashibara ibuki <ibuki.hayashibara@gmail.com>
2025-06-14 16:48:05 +0900

    a

--- udemy/material-for-windows/material-for-windows/front-dev-tutorial/04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
+++
@@ -1,7 +0,0 @@
-let name = 'Tom';
-console.log('Hello' + name);
-
-let variable = 'str';
-variable = 12;
-console.log(typeof variable);
-
--- udemy/material-for-windows/material-for-windows/front-dev-tutorial/04_JavaScriptの基礎を固める/040_配列の使い方/start/main.js
+++
@@ -1,7 +0,0 @@
-const array = [1,2,3,4,5,6,'moji'];
-
-array.push('new item');
-console.log(array.length);
-array.unshift('but');
-const val1 = array.pop();
-const val2 = array.shift();
